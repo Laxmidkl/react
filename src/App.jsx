e92a4596e6714822cb6138c0c56e295a957739f2
laxmidkl <dhakalaxmi74@gmail.com>
2024-01-16 09:17:45 +0545

    here

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='bg-green-500 text-5xl text-white w-56'>
+     Laxmi
+    </div>
+  )
+}
+
+export default App
