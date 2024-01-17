4f3cee5941d778c96e0af595bcc1ba8307a312a6
laxmidkl <dhakalaxmi74@gmail.com>
2024-01-17 08:46:21 +0545

    here

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,29 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='bg-green-500 text-5xl text-white w-56'>
-     Laxmi
+    <div className='bg-white-500 text-l text-green-950 '>
+
+      <div className='flex gap-16 h-20 relative left-96 top-7'>
+        <div>Home</div>
+        <div>About</div>
+        <div>service</div>
+        <div>Blog</div>
+        <div>contact</div>
+      </div>
+
+
+      <div className="h-screen w-full bg-cover"
+
+        style={{
+          backgroundImage: `url(https://images.pexels.com/photos/2165688/pexels-photo-2165688.jpeg?cs=srgb&dl=pexels-quang-nguyen-vinh-2165688.jpg&fm=jpg)`,
+        }}
+
+
+      >
+      </div>
+
+
+
     </div>
   )
 }
